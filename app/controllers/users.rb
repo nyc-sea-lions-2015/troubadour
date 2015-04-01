cc713c561e3195520431542cc067fe522e339613
Dan Asselin <dasselin86@gmail.com>
2015-04-01 12:20:46 -0400

    Fix bug in users/edit.erb form so request routes properly to PUT users/:id

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,7 +1,12 @@
+put '/users/:id' do
+  puts params
+end
+
 get '/users/:id/edit' do
   cur_user = User.find(session[:user_id])
   erb :'users/edit', locals: {user: cur_user}
 end
+
 
 get '/users/:id' do
   cur_user = User.find(params[:id])
