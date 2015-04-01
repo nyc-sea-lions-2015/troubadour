de35073648ea78e489dbb352a6a1f48091dcfc90
Dan Asselin <dasselin86@gmail.com>
2015-04-01 09:32:31 -0400

    app data skeleton migrated to database

--- db/migrate/20150401092328_create_favorites.rb
+++ db/migrate/20150401092328_create_favorites.rb
@@ -1,4 +1,10 @@
 class CreateFavorites < ActiveRecord::Migration
   def change
+    create_table :favorites do |t|
+      t.references :user
+      t.references :song
+
+      t.timestamps null: false
+    end
   end
 end
