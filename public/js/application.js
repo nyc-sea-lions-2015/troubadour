8d5a6a45243c7856a1871083c362dfc45f059c8c
Dan Asselin <dasselin86@gmail.com>
2015-04-01 17:29:01 -0400

    comment out nonworking ajax call in application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-  $('.favorite_form').on('submit', function(e){
-    e.preventDefault();
-    $.ajax({
-      type: $(e.target).attr('method'),
-      url: $(e.target).attr('action')
-    }).done(function(response){
-      console.log(response);
-    });
-  });
-});
+// $(document).ready(function() {
+//   $('.favorite_form').on('submit', function(e){
+//     e.preventDefault();
+//     $.ajax({
+//       type: $(e.target).attr('method'),
+//       url: $(e.target).attr('action')
+//     }).done(function(response){
+//       console.log(response);
+//     });
+//   });
+// });
