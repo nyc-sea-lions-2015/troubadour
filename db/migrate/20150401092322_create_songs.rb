e723fa9d47dd90687bfeb06fe32266a71440314d
Dan Asselin <dasselin86@gmail.com>
2015-04-01 09:30:45 -0400

    Write migration code for songs table

--- db/migrate/20150401092322_create_songs.rb
+++ db/migrate/20150401092322_create_songs.rb
@@ -1,4 +1,11 @@
 class CreateSongs < ActiveRecord::Migration
   def change
+    create_table :songs do |t|
+      t.references :user
+      t.string :title, null: false, limit: 256
+      t.text :content, null: false
+
+      t.timestamps null: false
+    end
   end
 end
