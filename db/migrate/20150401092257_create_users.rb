8078c4ea31991de7cce1ffe7dcd1b263433bb23b
Dan Asselin <dasselin86@gmail.com>
2015-04-01 09:28:51 -0400

    Write migration code for users table

--- db/migrate/20150401092257_create_users.rb
+++ db/migrate/20150401092257_create_users.rb
@@ -1,4 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :user_name, null: false, unique: true, limit: 56
+      t.string :password_digest
+
+      t.timestamps null: false
+    end
   end
 end
